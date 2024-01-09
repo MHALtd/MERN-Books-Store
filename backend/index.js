f755807672cfbdedd3c3eab32adecd246b3cf672
MHALtd <hasbi1117@gmail.com>
2024-01-10 05:34:13 +0700

    add route for update book, get all books and get one book

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
@@ -27,6 +29,58 @@ app.post("/books", async (request, response) => {
     const book = await Book.create(newBook);
 
     return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for get all books from database
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for get one books from database
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Update a Book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (!request.body.title || !request.body.author || !request.body.publishYear) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).send({ message: "Book not found" });
+    }
+    return response.status(200).send({ message: "Book updated succesfully" });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
