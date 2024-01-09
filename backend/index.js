5818f0cd8ce9c30ea7011f6951f4aa8f884961e0
MHALtd <hasbi1117@gmail.com>
2024-01-10 04:06:50 +0700

    remove config.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+// Route for Save a New Book
+app.post("/books", async (request, response) => {
+  try {
+    if (!request.body.title || !request.body.author || !request.body.publishYear) {
+    }
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
