7b9290e910f0bb2676649bb1c242e63a2b0491d1
MHALtd <hasbi1117@gmail.com>
2024-01-13 14:04:18 +0700

    improve ui using daisyUI

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineDelete } from "react-icons/md";
+import { CiMenuKebab } from "react-icons/ci";
 
 const BooksTable = ({ books }) => {
   return (
@@ -12,7 +13,6 @@ const BooksTable = ({ books }) => {
           <th className="border border-slate-600 rounded-md">Title</th>
           <th className="border border-slate-600 rounded-md max-md:hidden">Author</th>
           <th className="border border-slate-600 rounded-md max-md:hidden">Publish Year</th>
-          <th className="border border-slate-600 rounded-md">Operation</th>
         </tr>
       </thead>
       <tbody>
@@ -22,17 +22,37 @@ const BooksTable = ({ books }) => {
             <td className="border border-slate-600 rounded-md text-center">{book.title}</td>
             <td className="border border-slate-600 rounded-md text-center max-md:hidden">{book.author}</td>
             <td className="border border-slate-600 rounded-md text-center max-md:hidden">{book.publishYear}</td>
-            <td className="border border-slate-600 rounded-md text-center">
-              <div className="flex justify-center gap-x-4">
-                <Link to={`/books/details/${book._id}`}>
-                  <BsInfoCircle className="text-2xl text-green-800"></BsInfoCircle>
-                </Link>
-                <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className="text-2xl text-yellow-600"></AiOutlineEdit>
-                </Link>
-                <Link to={`/books/delete/${book._id}`}>
-                  <MdOutlineDelete className="text-2xl text-red-600"></MdOutlineDelete>
-                </Link>
+            <td className="text-center">
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn btn-outline">
+                  <CiMenuKebab />
+                </div>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
+                  <li>
+                    <div>
+                      <p>Info</p>
+                      <Link to={`/books/details/${book._id}`}>
+                        <BsInfoCircle className="text-2xl text-green-800"></BsInfoCircle>
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <p>Edit</p>
+                      <Link to={`/books/edit/${book._id}`}>
+                        <AiOutlineEdit className="text-2xl text-yellow-600"></AiOutlineEdit>
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <p>Delete</p>
+                      <Link to={`/books/delete/${book._id}`}>
+                        <MdOutlineDelete className="text-2xl text-red-600"></MdOutlineDelete>
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </td>
           </tr>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { MdOutlineAddBox } from "react-icons/md";
 import BooksCard from "../components/home/BooksCard";
 import BooksTable from "../components/home/BooksTable";
+import Navbar from "../components/home/Navbar";
 
 const Home = () => {
   const [books, setBooks] = useState([]);
@@ -27,18 +28,34 @@ const Home = () => {
 
   return (
     <div className="p-4">
-      <div className="flex justify-center items-center gap-x-4">
-        <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg" onClick={() => setShowType("table")}>
-          Table
-        </button>
-        <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg" onClick={() => setShowType("card")}>
-          Card
-        </button>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Books Store</a>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn">
+              Change View
+            </div>
+            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box space-y-2">
+              <li>
+                <button className="btn btn-outline text-center" onClick={() => setShowType("table")}>
+                  Table
+                </button>
+              </li>
+              <li>
+                <button className="btn btn-outline text-center" onClick={() => setShowType("card")}>
+                  Card
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
         <Link to="/books/create">
-          <MdOutlineAddBox className="text-sky-800 text-4xl"></MdOutlineAddBox>
+          <MdOutlineAddBox className="text-primary text-4xl"></MdOutlineAddBox>
         </Link>
       </div>
       {loading ? <Spinner /> : showType === "table" ? <BooksTable books={books} /> : <BooksCard books={books} />}
