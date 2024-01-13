af5c91f1d270cabede595ee76bba5089a8d8b00b
MHALtd <hasbi1117@gmail.com>
2024-01-13 10:02:28 +0700

    change localhost to deployed server

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/books/${id}`)
+      .delete(`https://mern-books-store.vercel.app/:5555/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book Deleted successfully", { variant: "success" });
